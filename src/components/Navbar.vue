cd9fdecd25259475a4e1c4762e478c48862e1a13
mulkimrwn <mulkimrwn@gmail.com>
2021-04-20 17:12:42 +0700

    done login

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../assets/ongkir.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      Cek Ongkir
+    </a>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <Navbar></Navbar>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    Navbar
+  }
+}
+</script>
