54dc617d2f16fe52b3d61a6e0712335fda045387
Hansel <loshaless@gmail.com>
2021-04-22 20:41:13 +0700

    revisi link AXIOS dari localhost menjadi heroku

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://book-lovers-hansel.herokuapp.com'
 })
 // 'http://localhost:3000'
 // 'https://book-lovers-hansel.herokuapp.com'
