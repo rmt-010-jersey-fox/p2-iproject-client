b7267fa73d35c85bd35f6131651c981aa63fcb2c
Muhammad Ihsan <muhammadihsan076@gmail.com>
2021-04-23 04:47:18 +0700

    to prepare add new feature (favorite surah)

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://qurantara-server.herokuapp.com/'
-  // baseURL: 'http://localhost:3000/'
+  // baseURL: 'https://qurantara-server.herokuapp.com/'
+  baseURL: 'http://localhost:3000/'
 })
