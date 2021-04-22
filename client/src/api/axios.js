6a39d1f7f66e55d755ee4f11af4981c594c83c29
dim-27 <dimasfirmanda27@gmail.com>
2021-04-22 12:43:49 +0700

    fixing footer

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:3000'
-  // baseURL: 'https://hospital-system-by-dimas.herokuapp.com/'
+  // baseURL: 'http://localhost:3000'
+  baseURL: 'https://hospital-system-by-dimas.herokuapp.com/'
 })
