5df3445fe9bb20787eaf32d27a1de0102a5bd2bd
ahmad-hanafi <57187761+ahmad-hanafi@users.noreply.github.com>
2021-04-23 13:35:48 +0700

    homescreen landing page

--- src/components/LeftContent.vue
+++ src/components/LeftContent.vue
@@ -1,33 +1,93 @@
 <template>
-  <div class="card mb-3" style="width: 700px; height: 700px">
-    <div class="card-header">
-      <h4>Nama user</h4>
-    </div>
-    <img src="http://arah.in/memes-8" class="card-img-top"  alt="..." />
-    <div class="card-body">
-      <h5 class="card-title">Ini adalah title</h5>
-      <h5 class="card-subtitle mb-2">
-        <small><i>Posted on February 19, 2016</i></small>
-      </h5>
-      <button class="btn btn-outline-success">
-        <i class="far fa-thumbs-up"></i> Love it
-      </button>
-      <button class="btn btn-outline-danger">
-        <i class="far fa-thumbs-down"></i> Hate it
-      </button>
-      <button class="btn btn-warning">
-        <i class="fas fa-bullhorn"></i> Report
-      </button>
-      <p class="card-text">
-        <small class="text-muted">You, Andi wakwaw and 20 people love it</small>
-      </p>
+  <div class="container">
+    <div class="row">
+      <div
+        class="card mb-3 mt-3 mr-3 ml-3"
+        style="width: 700px; height: 700px"
+        v-for="list in memes"
+        :key="list.id"
+      >
+        <div class="card-header">
+          <h4>{{list.User.name}}</h4>
+        </div>
+        <img :src="list.image_url" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">{{list.title}}</h5>
+          <h5 class="card-subtitle mb-2">
+            <small><i>Posted on {{changeToString(list.createdAt)}}</i></small>
+          </h5>
+          <button class="btn btn-outline-success" @click.prevent="like(list.id)">
+             Love it
+          </button>
+          <!-- <button class="btn btn-success" v-if="like">
+             Love it
+          </button> -->
+          <button class="btn btn-outline-danger" @click.prevent="dislike(list.id)">
+             Hate it
+          </button>
+          <!-- <button class="btn btn-danger">
+             Hate it
+          </button> -->
+          <button class="btn btn-outline-info" @click.prevent="report(list.id)">
+             Report
+          </button>
+          <!-- <button class="btn btn-info">
+             Report
+          </button> -->
+          <p class="card-text">
+            <small class="text-muted"
+              >{{list.likes}} People loved this</small
+            >
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  methods: {
+    getMeme() {
+      this.$store.dispatch("getMemes");
+    },
+    getMemeById(id) {
+      this.$store.dispatch("getMemeById", {
+        id: id,
+        router: this.$router,
+      });
+    },
+    changeToString(date) {
+      const dataDate = new Date(date);
+      return dataDate.toDateString();
+    },
+    like(id) {
+      this.$store.dispatch("like", {
+        id: id,
+        router: this.$router,
+      });
+    },
+    dislike(id) {
+      this.$store.dispatch("dislike", {
+        id: id,
+        router: this.$router,
+      });
+    },
+    report(id) {
+      this.$store.dispatch("report", {
+        id: id,
+        router: this.$router,
+      });
+    },
+  },
+  mounted() {
+    this.getMeme();
+  },
+  computed: {
+    memes() {
+      return this.$store.state.memes;
+    },
+  },
 };
 </script>
 
--- src/components/RightContent.vue
+++ src/components/RightContent.vue
@@ -2,15 +2,22 @@
   <div>
     <div class="btn-group-vertical btn-group-lg">
     <h1>Arrange by: </h1>
-      <button class="btn btn-success">Default</button>
-      <button class="btn btn-success">Populer</button>
+      <button class="btn btn-success" @click.prevent="getMeme">Default</button>
+      <button class="btn btn-success" @click.prevent="getMemesHot">Populer</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    getMeme() {
+      this.$store.dispatch("getMemes");
+    },
+    getMemesHot() {
+      this.$store.dispatch("getMemesHot");
+    }
+  }
 };
 </script>
 
