161bd29beb507b003db5495f0e6a48f3d283e00c
syauqy <syauqilenterano@gmail.com>
2021-04-23 10:08:14 +0700

    search fitur

---
+++ src/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h3 class="subtitle">With Material Design Icons</h3>
+    <form @submit.prevent="onSubmit">
+      <b-field>
+        <b-input
+          placeholder="Search..."
+          type="search"
+          icon="magnify"
+          icon-clickable
+          @icon-click="searchIconClick"
+        >
+        </b-input>
+      </b-field>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  methods: {
+    searchIconClick() {
+      alert("You wanna make a search?");
+    },
+    clearIconClick() {
+      this.email = "";
+      alert("Email cleared!");
+    },
+  },
+};
+</script>
